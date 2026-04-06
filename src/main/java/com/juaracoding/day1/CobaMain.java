16a412a6779746611eb92cac2c827c0cabe71a20
pollchihuy <poll.chihuy@gmail.com>
2026-04-06 21:42:28 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/day1/CobaMain.java
@@ -0,0 +1,17 @@
+package com.juaracoding.day1;
+
+public class CobaMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println();
+        for (int i = 0;
+             i < 3;
+             i++) {
+            System.out.println(i);
+        }
+        if(true){
+            System.out.println("OK");
+        }
+    }
+}
---
+++ src/main/java/com/juaracoding/day1/DeklarasiVariabel.java
@@ -0,0 +1,32 @@
+package com.juaracoding.day1;
+
+//ini adalah ke 1
+// ini adalah ke 2
+public class DeklarasiVariabel {
+
+    /**
+     * ini adalah fungsi untuk bla bla bla
+     * @param args
+     */
+    public static void main(String[] args) {
+        //ini deklarasi
+        //tipe data primitive int,double, short,byte
+        // long , float, char, boolean
+        int intX;
+        //inisialisasi
+        intX = 2;
+        //deklarasi sekaligus inisialisasi
+        int intY = 3;
+        int intA,intB,intC,intD;//ini sangat salah !!
+        int intE;
+        int intF;
+        @Deprecated
+        int intG;
+//        int intZ = 5/2;
+        double intZ = 5/2.0;
+        System.out.println("Nilai intZ : "+intZ);
+
+        byte b = 127 ;
+
+    }
+}
